4c213c9c8aef00977df35dad7ddb0a06b9b17cd0
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-04-23 16:22:18 -0300

    HTML & CSS static template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,105 @@
-import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="container">
+        <section className="filter">
+          <h3>Filtros</h3>
+          <label>
+            <span>Valor mínimo:</span>
+            <input type="number" />
+          </label>
+          <label>
+            <span>Valor máximo:</span>
+            <input type="number" />
+          </label>
+          <label>
+            <span>Busca por nome:</span>
+            <input type="number" />
+          </label>
+        </section>
+
+        <section className="list">
+          <header>
+            <p>Quantidade de produtos: 3</p>
+            <label>
+              <span>Ordenação</span>
+              <select>
+                <option value="cresc">Crescente</option>
+                <option value="descr">Decrescente</option>
+              </select>
+            </label>
+          </header>
+
+          <div className="grid">
+            <div className="product">
+              <img src="https://picsum.photos/200/200?a=3" alt="product" />
+              <div className="info">
+                <p>Produto Legal</p>
+                <p>R$123,00</p>
+                <button>Adicionar ao Carrinho</button>
+              </div>
+            </div>
+            <div className="product">
+              <img src="https://picsum.photos/200/200?a=3" alt="product" />
+              <div className="info">
+                <p>Produto Legal</p>
+                <p>R$123,00</p>
+                <button>Adicionar ao Carrinho</button>
+              </div>
+            </div>
+            <div className="product">
+              <img src="https://picsum.photos/200/200?a=3" alt="product" />
+              <div className="info">
+                <p>Produto Legal</p>
+                <p>R$123,00</p>
+                <button>Adicionar ao Carrinho</button>
+              </div>
+            </div>
+            <div className="product">
+              <img src="https://picsum.photos/200/200?a=3" alt="product" />
+              <div className="info">
+                <p>Produto Legal</p>
+                <p>R$123,00</p>
+                <button>Adicionar ao Carrinho</button>
+              </div>
+            </div>
+            <div className="product">
+              <img src="https://picsum.photos/200/200?a=3" alt="product" />
+              <div className="info">
+                <p>Produto Legal</p>
+                <p>R$123,00</p>
+                <button>Adicionar ao Carrinho</button>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="cart">
+          <h3>Carrinho</h3>
+          <div className="products">
+            <div>
+              <span>1x</span>
+              <span>Produto Legal</span>
+              <button>Remover</button>
+            </div>
+            <div>
+              <span>3x</span>
+              <span>Produto Normal</span>
+              <button>Remover</button>
+            </div>
+            <div>
+              <span>2x</span>
+              <span>Produto Foda</span>
+              <button>Remover</button>
+            </div>
+          </div>
+          <p>Valor total: R$100,00</p>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default App;
