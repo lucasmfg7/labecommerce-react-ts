586928a0b960c6a7cf8ab3e6cfd317772fd3244e
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-04-23 16:49:20 -0300

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
+import styled from "styled-components";
 import { Filter } from "./components/Filter";
-import "./App.css";
 import { List } from "./components/List";
 import { Cart } from "./components/Cart";
 
+export const Container = styled.div`
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+`;
+
 const App = () => {
   return (
-    <>
-      <div className="container">
-        <Filter />
-
-        <List />
-
-        <Cart />
-      </div>
-    </>
+    <Container>
+      <Filter />
+      <List />
+      <Cart />
+    </Container>
   );
 };
 
